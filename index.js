ea9c2adb5d269845ff447c5fa747fea1c415b27c
somaiya shahin <somaiyashahin484@gmail.com>
2023-05-20 01:13:54 +0600

    added addToys routes

--- index.js
+++ index.js
@@ -29,8 +29,18 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
+
+    app.post("/addToys", async(req, res) =>{
+        const body = req.body;
+
+        
+        console.log(body);
+    })
+
+
   } finally {
-    // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
