dfe827fafde0c148539621a315cca7e61a3773df
somaiya shahin <somaiyashahin484@gmail.com>
2023-05-20 16:02:20 +0600

    get alltoys data

--- index.js
+++ index.js
@@ -15,43 +15,54 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        const allAddedToys = client.db('toyStore').collection('addToys')
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
 
+        app.post("/addToys", async (req, res) => {
+            const body = req.body;
+            if (!body) {
+                return res.status(404).send({ message: "Not valid request" })
+            }
 
-    app.post("/addToys", async(req, res) =>{
-        const body = req.body;
+            const result = await allAddedToys.insertOne(body);
 
-        
-        console.log(body);
-    })
+            console.log(result);
+            res.send(result)
+        });
+
+        app.get("/allToys", async(req, res) =>{
+            const result = await allAddedToys.find({}).toArray();
+            res.send(result)
+        })
 
 
-  } finally {
-    await client.close();
-  }
+    } finally {
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.send('Toy shop is on the way')
 })
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log(`Toy shop is running on port ${port}`)
 })
